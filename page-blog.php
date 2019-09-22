834b6afd9111eec9f2042e387bd41b6daf626a58
mloweedgar <mloweedgar0@gmail.com>
2019-09-22 14:50:20 +0300

    feat: add news and blog paage

---
+++ page-blog.php
@@ -0,0 +1,92 @@
+<?php
+wp_enqueue_style('blog', get_template_directory_uri() . '/css/blog.css');
+get_header();
+
+?>
+
+<section id="banner">
+    <div class="section-wrapper">
+        <h2>
+            <small>OUR BLOG</small>
+            Show me <a href="#">everything.</a>
+        </h2>
+    </div>
+</section>
+
+<section id="blogs">
+    <div class="section-wrapper">
+        <div class="blog-item">
+            <div class="image">
+                <img src="https://images.unsplash.com/photo-1532251632967-758f04b1c338?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="">
+            </div>
+
+            <div class="text">
+                <strong>FROM OUR BLOG</strong>
+                <h2>
+                    Mapping Manzese
+                </h2>
+                <p class="for-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, nam unde iusto quas veniam in, consequatur, id corrupti dolore nihil velit non eum consequuntur minima architecto. Molestias quos porro corporis?</p>
+                <p class="for-mob">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, nam unde iusto quas veniam in, consequatur...
+                </p>
+                <a href="#">LEARN MORE</a>
+            </div>
+        </div>
+
+        <div class="blog-item">
+            <div class="image">
+                <img src="https://images.unsplash.com/photo-1532251632967-758f04b1c338?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="">
+            </div>
+
+            <div class="text">
+                <strong>FROM OUR BLOG</strong>
+                <h2>
+                    Mapping Manzese
+                </h2>
+                <p class="for-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, nam unde iusto quas veniam in, consequatur, id corrupti dolore nihil velit non eum consequuntur minima architecto. Molestias quos porro corporis?</p>
+                <p class="for-mob">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, nam unde iusto quas veniam in, consequatur...
+                </p>
+                <a href="#">LEARN MORE</a>
+            </div>
+        </div>
+
+        <div class="blog-item">
+            <div class="image">
+                <img src="https://images.unsplash.com/photo-1532251632967-758f04b1c338?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="">
+            </div>
+
+            <div class="text">
+                <strong>FROM OUR BLOG</strong>
+                <h2>
+                    Mapping Manzese
+                </h2>
+                <p class="for-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, nam unde iusto quas veniam in, consequatur, id corrupti dolore nihil velit non eum consequuntur minima architecto. Molestias quos porro corporis?</p>
+                <p class="for-mob">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, nam unde iusto quas veniam in, consequatur...
+                </p>
+                <a href="#">LEARN MORE</a>
+            </div>
+        </div>
+
+        <div class="blog-item">
+            <div class="image">
+                <img src="https://images.unsplash.com/photo-1532251632967-758f04b1c338?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="">
+            </div>
+
+            <div class="text">
+                <strong>FROM OUR BLOG</strong>
+                <h2>
+                    Mapping Manzese
+                </h2>
+                <p class="for-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, nam unde iusto quas veniam in, consequatur, id corrupti dolore nihil velit non eum consequuntur minima architecto. Molestias quos porro corporis?</p>
+                <p class="for-mob">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, nam unde iusto quas veniam in, consequatur...
+                </p>
+                <a href="#">LEARN MORE</a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
