4224cf01c727edfb0522f9ac048c325e44fb07a5
mloweedgar <mloweedgar0@gmail.com>
2019-09-23 14:57:02 +0300

    feat: add undercontruction page for pages that are still undercontruction

--- single.php
+++ single.php
@@ -1,15 +1,31 @@
-<?php
-//wp_enqueue_style('blog', get_template_directory_uri() . '/css/blog.css');
-get_header();
 
-?>
+<?php get_header(); ?>
+<style>
+    .underconstruction {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin: 10% auto 20% auto;
+    }
+    .underconstruction h1 {
+        font-size: 4em;
+        padding: 20px;
+        text-align: center;
+        letter-spacing: 2px;
+    }
 
-<?php
-if ( have_posts() ) : while ( have_posts() ) : the_post();
+    .underconstruction p {
+        text-align: center;
+        line-height: 2;
+    }
+</style>
 
-    get_template_part( 'content-single', get_post_format() );
-
-endwhile; endif;
-?>
+<div class="underconstruction">
+    <div>
+        <h1>Under Construction</h1>
+        <p>This page is underconstruction, our team of web developers are working hard to bring this page up as soon as possible  </p>
+        <p>Please continue to visit other pages on our site!</p>
+    </div>
+</div>
 
 <?php get_footer(); ?>
