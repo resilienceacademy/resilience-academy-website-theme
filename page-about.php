ada89e174e8965bb0fc80795d2e8df67c550c16f
mloweedgar <mloweedgar0@gmail.com>
2019-10-15 09:33:52 +0300

    feat: add our team section

--- page-about.php
+++ page-about.php
@@ -169,6 +169,50 @@ get_header();
         </div>
     </div>
 </section>
+<section id="ourCommunity">
+    <div class="section-wrapper">
+        <div>
+            <h2>OUR COMMUNITY</h2>
+            <div id="moreAbout" class="layout center-justified wrap">
+                <div>
+                    <div class="decorated-inset-box blue-bg">
+                        <div>
+                            <h3>Team</h3>
+                            <p>Get to know us</p>
+                            <a href="#" class="button">
+                                Learn More
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div>
+                    <div class="decorated-inset-box green-bg">
+                        <div>
+                            <h3>Institutions</h3>
+                            <p>Meet our members</p>
+                            <a href="#" class="button">
+                                Learn More
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div>
+                    <div class="decorated-inset-box yellow-bg">
+                        <div>
+                            <h3>Partners</h3>
+                            <p>Meet our partners</p>
+                            <a href="#" class="button">
+                                Learn More
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
 <script>
     var methodsScroller = document.querySelector("#methodsScroller");
