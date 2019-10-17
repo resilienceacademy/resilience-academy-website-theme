881f8cb3a811f809fd408830ddce0feca3b657b7
mloweedgar <mloweedgar0@gmail.com>
2019-10-17 15:06:47 +0300

    feat: add styles for view articles

--- page-repository.php
+++ page-repository.php
@@ -1,9 +1,13 @@
-<?php get_header(); ?>
-<section style="margin-top: 10%">
+<?php
+//wp_enqueue_style('repository', get_template_directory_uri() . '/css/repository.css');
+get_header();
+?>
+<section id="repository">
+    <h2>Research Repository</h2>
     <?php
     if ( have_posts() ) : while ( have_posts() ) : the_post();
 
-        get_template_part( 'partials/content-feed', get_post_format() );
+        get_template_part( 'partials/content-article', get_post_format() );
 
     endwhile; endif;
     ?>
---
+++ partials/content-article.php
@@ -0,0 +1,3 @@
+<div class="articles-content">
+    <?php the_content(); ?>
+</div>
