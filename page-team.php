9c1909ccf4031d9e3872972c3856dd28c143f37a
mloweedgar <mloweedgar0@gmail.com>
2019-10-19 00:46:34 +0300

    feat: add layout for our team page

---
+++ page-team.php
@@ -0,0 +1,192 @@
+<?php
+wp_enqueue_style('team', get_template_directory_uri() . '/css/team.css');
+get_header();
+
+?>
+
+    <section id="banner">
+        <div class="section-wrapper">
+            <h2>
+                Resilience begins with a team of dedicated and brilliant problem solvers.
+            </h2>
+        </div>
+    </section>
+
+    <section id="team">
+        <div class="section-wrapper">
+            <div class="layout wrap">
+                <div class="team-member">
+                    <div class="image">
+                        <img src="https://resilienceacademy.ac.tz/wp-content/uploads/2019/06/Msilikale-Msilanga-with-Sout-500x500.jpg" alt="">
+                    </div>
+
+                    <div class="text for-mob">
+                        <h3>Msilikale Msilanga</h3>
+                        <h5>Project Researcher</h5>
+                    </div>
+                </div>
+
+                <div class="team-member for-lg">
+                    <div class="text">
+                        <h3>Msilikale Msilanga</h3>
+                        <h5>Project Researcher</h5>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
+                        </p>
+
+                        <ul class="team-member-links layout center">
+                            <li class="layout center-center">
+                                <a href="https://linkedin.com" title="Follow on Linked In" target="_blank">
+                                    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect x="2" y="9" width="4" height="12"></rect><circle cx="4" cy="4" r="2"></circle></svg>
+                                </a>
+                            </li>
+                            <li id="twitterLink" class="layout center-center">
+                                <a href="https://twitter.com" title="Follow on Twitter" target="_blank">
+                                    <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Twitter icon</title><path d="M23.954 4.569c-.885.389-1.83.654-2.825.775 1.014-.611 1.794-1.574 2.163-2.723-.951.555-2.005.959-3.127 1.184-.896-.959-2.173-1.559-3.591-1.559-2.717 0-4.92 2.203-4.92 4.917 0 .39.045.765.127 1.124C7.691 8.094 4.066 6.13 1.64 3.161c-.427.722-.666 1.561-.666 2.475 0 1.71.87 3.213 2.188 4.096-.807-.026-1.566-.248-2.228-.616v.061c0 2.385 1.693 4.374 3.946 4.827-.413.111-.849.171-1.296.171-.314 0-.615-.03-.916-.086.631 1.953 2.445 3.377 4.604 3.417-1.68 1.319-3.809 2.105-6.102 2.105-.39 0-.779-.023-1.17-.067 2.189 1.394 4.768 2.209 7.557 2.209 9.054 0 13.999-7.496 13.999-13.986 0-.209 0-.42-.015-.63.961-.689 1.8-1.56 2.46-2.548l-.047-.02z"/></svg>
+                                </a>
+                            </li>
+                            <li class="layout center-center">
+                                <a href="https://www.facebook.com" title="Follow on Facebook" target="_blank">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 480 480"><defs><clipPath clipPathUnits="userSpaceOnUse"><path d="M0 48 48 48 48 0 0 0 0 48Z"/></clipPath><clipPath clipPathUnits="userSpaceOnUse"><path d="M0 48 48 48 48 0 0 0 0 48Z"/></clipPath><clipPath clipPathUnits="userSpaceOnUse"><path d="M0 48 48 48 48 0 0 0 0 48Z"/></clipPath><clipPath clipPathUnits="userSpaceOnUse"><path d="M0 48 48 48 48 0 0 0 0 48Z"/></clipPath><clipPath clipPathUnits="userSpaceOnUse"/><clipPath clipPathUnits="userSpaceOnUse"/><clipPath clipPathUnits="userSpaceOnUse"><path d="M48 48 0 48 0 0 48 0 48 48Z"/></clipPath><clipPath clipPathUnits="userSpaceOnUse"><path d="M0 48 48 48 48 0 0 0 0 48Z"/></clipPath><clipPath clipPathUnits="userSpaceOnUse"/><clipPath clipPathUnits="userSpaceOnUse"/><clipPath clipPathUnits="userSpaceOnUse"><path d="M0 0 48 0 48 48 0 48 0 0Z"/></clipPath></defs><g transform="matrix(1.25 0 0 -1.25 0 480)"><path d="m218 32 0 146 49.5 0 7.4 56.9-56.9 0 0 36.3c0 16.5 4.6 27.7 28.5 27.7l30.5 0 0 50.9C271.6 350.5 253.6 352 232.5 352c-43.9 0-74-26.5-74-75.2l0-42-49.7 0 0-56.9 49.7 0 0-146 59.4 0z"/></g></svg>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="team-member">
+                    <div class="image">
+                        <img src="https://resilienceacademy.ac.tz/wp-content/uploads/2019/06/Msilikale-Msilanga-with-Sout-500x500.jpg" alt="">
+                    </div>
+
+                    <div class="text">
+                        <h3>Msilikale Msilanga</h3>
+                        <h5>Project Researcher</h5>
+                    </div>
+                </div>
+
+
+                <div class="team-member">
+                    <div class="image">
+                        <img src="https://resilienceacademy.ac.tz/wp-content/uploads/2019/06/Msilikale-Msilanga-with-Sout-500x500.jpg" alt="">
+                    </div>
+
+                    <div class="text">
+                        <h3>Msilikale Msilanga</h3>
+                        <h5>Project Researcher</h5>
+                    </div>
+                </div>
+
+                <div class="team-member">
+                    <div class="image">
+                        <img src="https://resilienceacademy.ac.tz/wp-content/uploads/2019/06/Msilikale-Msilanga-with-Sout-500x500.jpg" alt="">
+                    </div>
+
+                    <div class="text">
+                        <h3>Msilikale Msilanga</h3>
+                        <h5>Project Researcher</h5>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </section>
+
+    <section id="methods">
+        <div class="section-wrapper position-relative">
+            <div class="overflow-hidden" style="background: #d2d2d2;">
+                <div id="methodsScroller" class="layout center">
+                    <div class="method-item layout">
+                        <img src="https://resilienceacademy.ac.tz/wp-content/uploads/2019/06/about-team.jpg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="">
+                    </div>
+                    <div class="method-item layout">
+                        <img src="https://resilienceacademy.ac.tz/wp-content/uploads/2019/06/about-team.jpg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="">
+                    </div>
+                    <div class="method-item layout">
+                        <img src="https://resilienceacademy.ac.tz/wp-content/uploads/2019/06/about-team.jpg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="">
+                    </div>
+                    <div class="method-item layout">
+                        <img src="https://resilienceacademy.ac.tz/wp-content/uploads/2019/06/about-team.jpg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="">
+                    </div>
+                    <div class="method-item layout">
+                        <img src="https://resilienceacademy.ac.tz/wp-content/uploads/2019/06/about-team.jpg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="">
+                    </div>
+                </div>
+            </div>
+
+            <div id="methodsMover" class="position-absolute pin-all pointer-events-none layout center justified">
+                <button onclick="moveMethods(-1)" class="action-button pointer-events-auto">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-left"><polyline points="15 18 9 12 15 6"></polyline></svg>
+                </button>
+
+                <button onclick="moveMethods(1)" class="action-button pointer-events-auto">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right"><polyline points="9 18 15 12 9 6"></polyline></svg>
+                </button>
+            </div>
+        </div>
+    </section>
+
+    <section id="aboutResilience">
+        <div class="section-wrapper">
+            <div>
+                <h2>Our Community</h2>
+
+                <div id="moreAbout" class="layout center-justified wrap">
+                    <div>
+                        <div class="decorated-inset-box green-bg">
+                            <div>
+                                <h3>Institutions</h3>
+                                <p>Meet our members</p>
+                                <a href="#" class="button">
+                                    Learn More
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div class="decorated-inset-box yellow-bg">
+                            <div>
+                                <h3>Partners</h3>
+                                <p>Meet our partners</p>
+                                <a href="#" class="button">
+                                    Learn More
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+<?php include('includes/contact-form.php'); ?>
+<?php include('includes/subscribe.php'); ?>
+
+    <script>
+        var methodsScroller = document.querySelector("#methodsScroller");
+        var methodsSlideIndex = 0;
+        var methodItems = document.querySelectorAll('.method-item');
+        var methodTabs = document.querySelectorAll('.method-tab');
+        function moveMethods(dir){
+            var newIndex = methodsSlideIndex + dir;
+            var canGoForward = newIndex >= 0 && dir === -1;
+            var canGoBack = newIndex < methodItems.length && dir === 1;
+            if(canGoBack || canGoForward){
+                doMethodsMovement(newIndex);
+            }
+        }
+        function doMethodsMovement(newIndex){
+            methodsSlideIndex = newIndex;
+            methodsScroller.style.transform = "translateX("+(-100 * newIndex)+"%)";
+            document.querySelector('.method-tab.active').classList.remove('active');
+            methodTabs[newIndex].classList.add('active');
+        }
+
+        function moveMethodsTo(e, index){
+            e.preventDefault();
+            doMethodsMovement(index);
+        }
+    </script>
+<?php get_footer(); ?>
