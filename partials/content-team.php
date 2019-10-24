a8c01d31500c8b3cb33c168082a43a151c637363
mloweedgar <mloweedgar0@gmail.com>
2019-10-24 14:55:16 +0300

    feat: re-design our team page and implement team detail page

--- partials/content-team.php
+++ partials/content-team.php
@@ -3,10 +3,18 @@
         <img src="<?php if ( has_post_thumbnail() ) {
             the_post_thumbnail_url();
         } ?>" alt="">
+        <div class="for-lg learn-more layout end center-justified">
+            <a href="<?php the_permalink(); ?>" class="button">
+                LEARN MORE
+            </a>
+        </div>
     </div>
 
     <div class="text">
         <h3><?php echo get_post_meta(get_the_ID(), 'name', true); ?></h3>
         <h5><?php echo get_post_meta(get_the_ID(), 'title', true); ?></h5>
+        <a href="<?php the_permalink(); ?>" class="button for-mob">
+            LEARN MORE
+        </a>
     </div>
 </div>
