9176a4f2a5ba4e850e89f800fcdc741aa029bfb2
mloweedgar <mloweedgar0@gmail.com>
2019-11-05 10:39:21 +0300

    feat: make partners and  institutions  logo clickable

--- partials/content-partner.php
+++ partials/content-partner.php
@@ -1,9 +1,9 @@
 
 
-<div class="featured-item">
+<a href="<?php the_permalink(); ?>" class="featured-item">
     <div class="image">
         <img src="<?php if ( has_post_thumbnail() ) {
             the_post_thumbnail_url();
         } ?>" alt="">
     </div>
-</div>
+</a>
